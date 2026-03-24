06014101cb5ceb98e6cc1f258e904ac069257924
Joydeep Bhowmik <45948068+joydeep-bhowmik@users.noreply.github.com>
2026-03-25 02:51:40 +0530

    Create shortcut.m

---
+++ zed/shortcut.m
@@ -0,0 +1,12 @@
+Ctrl + Shift + /   = Agent Panel
+Ctrl + Shift + I   = Edit Prediction
+Ctrl + B           = Project Panel
+Ctrl + Shift + F   = Project Search
+Ctrl + Shift + P   = Command Palette
+Ctrl + `           = Terminal
+Ctrl + Shift + `   = New Terminal
+Ctrl + Shift + G   = Git Panel
+
+Ctrl + Shift + C   = Colab Panel
+Alt + Shift + L    = Language Server
+Ctrl + Alt + B     = Notification
