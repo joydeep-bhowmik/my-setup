6a3fa61a0d1644a52b094482a7def23dbd1260de
Joydeep Bhowmik <45948068+joydeep-bhowmik@users.noreply.github.com>
2026-03-28 01:35:20 +0530

    Update and rename shortcut.md to shortcut.m

---
+++ zed/shortcut.m
@@ -0,0 +1,15 @@
+ Ctrl + Shift + /      = Agent Panel            
+ Ctrl + Space          = Code Suggesttion           
+ Ctrl + Shift + I      = Edit Prediction        
+ Ctrl + Shift + E      = Project Panel          
+ Ctrl + Shift + F      = Project Code Search    
+ Ctrl + E              = Project File Search    
+ Ctrl + B              = Toggle Right Doc       
+ Ctrl + Shift + B      = Toggle Left Doc        
+ Ctrl + Shift + P      = Command Palette        
+ Ctrl + `              = Terminal               
+ Ctrl + Shift + `      = New Terminal           
+ Ctrl + Shift + G      = Git Panel              
+ Ctrl + Shift + C      = Colab Panel            
+ Alt + Shift + L       = Language Server        
+ Ctrl + Alt + B        = Notifications          
